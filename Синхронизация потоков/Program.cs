91e6f88e3d5b9ea04b7880bd09defe9adf94372a
boronin_1993@mail.ru <boronin_1993@mail.ru>
2022-12-04 12:55:48 +0300

    Добавьте файлы проекта.

---
+++ Синхронизация потоков/Program.cs
@@ -0,0 +1,67 @@
+﻿using static System.Console;
+int x = 0;
+//object locker = new();
+//AutoResetEvent waitHandler = new AutoResetEvent(true);//объект-сигнал
+Mutex mutexObj = new ();
+for(int i=1;i<6;i++)
+{
+    Thread myThread = new(Print);
+    myThread.Name = $"Поток {i}";
+    myThread.Start();
+}
+/*//void Print()
+//{
+//    lock (locker)
+//    {
+//        x = 1;
+//        for (int i = 1; i < 6; i++)
+//        {
+//            WriteLine($"{Thread.CurrentThread.Name}:{x}");
+//            x++;
+//            Thread.Sleep(100);
+//        }
+//    }
+//}
+//void Print()
+//{
+//    bool acquiredLock = false;
+//    try
+//    {
+//        Monitor.Enter(locker, ref acquiredLock);
+//        x = 1;
+//        for (int i = 1; i < 6; i++)
+//        {
+//            WriteLine($"{Thread.CurrentThread.Name}:{x}");
+//            x++;
+//            Thread.Sleep(100);
+//        }
+//    }
+//    finally
+//    {
+//        if (acquiredLock) Monitor.Exit(locker);
+//    }
+////}
+//void Print()
+//{
+//    waitHandler.WaitOne();
+//    x = 1;
+//    for (int i = 1; i < 6; i++)
+//    {
+//        WriteLine($"{Thread.CurrentThread.Name}:{x}");
+//        x++;
+//        Thread.Sleep(100);
+//    }
+//    waitHandler.Set();//сигнализируем, что waitHandler в сигнальном состоянии
+//}*/
+void Print()
+{
+    mutexObj.WaitOne(); //пристанавливаем поток до получения мьютекса
+    x = 1;
+    for (int i = 1; i < 6; i++)
+    {
+        WriteLine($"{Thread.CurrentThread.Name}:{x}");
+        x++;
+        Thread.Sleep(100);
+    }
+    mutexObj.ReleaseMutex(); //освобождаем мьютекс
+}
